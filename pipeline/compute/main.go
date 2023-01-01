e610f41d120fbcbd2fad7cf3f107a92ca27fb462
Orel <ryiseld@gmail.com>
2023-01-01 13:07:46 +0200

    Compute: Send computed message to RabbitMQ

--- pipeline/compute/main.go
+++ pipeline/compute/main.go
@@ -3,21 +3,83 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"strconv"
+	"strings"
+	"time"
 
+	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/segmentio/kafka-go"
 )
 
 func main() {
+	// Initialize Kafka
 	reader := kafka.NewReader(kafka.ReaderConfig{
-		Brokers: []string{"127.0.0.1:29092"},
-		Topic:   "pipeline",
+		Brokers:     []string{"localhost:29092"},
+		Topic:       "pipeline",
+		StartOffset: -1,
 	})
 
+	// Initialize RabbitMQ
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ch.Close()
+	q, err := ch.QueueDeclare(
+		"compute", // name
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Receive Kafka messages
 	for {
 		m, err := reader.ReadMessage(context.Background())
 		if err != nil {
-			break
+			log.Fatal(err)
 		}
-		fmt.Printf("message at offset %d: %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+		log.Printf("[Kafka] Received (offset %d): %s = %s\n", m.Offset, string(m.Key), string(m.Value))
+		coords := strings.Split(string(m.Value), ",")
+		x, err := strconv.Atoi(coords[0])
+		if err != nil {
+			log.Fatal(err)
+		}
+		y, err := strconv.Atoi(coords[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Perform some computation
+		x *= 10
+		y *= 10
+
+		// Send message to RabbitMQ
+		body := fmt.Sprintf("%d,%d", x, y)
+		err = ch.PublishWithContext(ctx,
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "text/plain",
+				Body:        []byte(body),
+			})
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("[RabbitMQ] Sent: %s\n", body)
 	}
 }
