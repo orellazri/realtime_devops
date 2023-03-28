9e3eb6754125e584b1851315fea5c8ffd550e36d
Orel <ryiseld@gmail.com>
2023-03-28 10:18:05 +0300

    Performance: Add EMQX test

---
+++ performance/emqx/emqx_test.go
@@ -0,0 +1,32 @@
+package emqx
+
+import (
+	"testing"
+	"time"
+)
+
+func Setup(t *testing.T) *Connection {
+	t.Helper()
+
+	return NewConnection()
+}
+
+func TestWriteBenchmark(t *testing.T) {
+	conn := Setup(t)
+
+	var iterationsMap = map[int]time.Duration{
+		100:  time.Duration(100 * time.Millisecond),
+		1000: time.Duration(1000 * time.Millisecond),
+	}
+
+	for numIters, expectedTime := range iterationsMap {
+		totalTime, err := conn.BenchmarkWrite(numIters)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if totalTime > expectedTime {
+			t.Errorf("total write time is too long. expected %v got %v", expectedTime, totalTime)
+		}
+	}
+}
