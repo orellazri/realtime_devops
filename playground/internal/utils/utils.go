4f38cb6e50a498ea7406246201995c229d235256
Orel <ryiseld@gmail.com>
2023-04-13 14:13:55 +0300

    Playground: Add custom error for no message available

--- playground/internal/utils/utils.go
+++ playground/internal/utils/utils.go
@@ -15,3 +15,12 @@ type Message struct {
 func GetMessageTime(message Message) time.Duration {
 	return message.Received.Sub(message.Sent)
 }
+
+
+type NoMessageError struct {}
+func (e *NoMessageError) Error() string {
+	return "no message available yet"
+}
+func (e *NoMessageError) Is(target error) bool {
+        return e.Error() == target.Error()
+}
